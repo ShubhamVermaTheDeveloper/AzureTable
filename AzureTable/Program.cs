75e2934e2350408ce5af6f1581d9abfee0577836
shubham verma <101853403+Shubham25122001@users.noreply.github.com>
2023-10-09 14:21:52 +0530

    Insert batch Entiy into the table

---
+++ AzureTable/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+
+namespace AzureTable
+{
+    class Program
+    {
+        private static string connection_stri = "DefaultEndpointsProtocol=https;AccountName=shubham12;AccountKey=AX7b/yGFhfb4f8/eMqX724KfvPlf9ekENK4cBe9wPA5+7FAKc+EzPvubBi42KuWMWGoRZ16GT17z+AStzzbKgA==;EndpointSuffix=core.windows.net";
+        private static string table_name = "Customer";
+        static void Main(string[] args)
+        {
+            CloudStorageAccount _accout = CloudStorageAccount.Parse(connection_stri);
+           
+            CloudTableClient _table_client = _accout.CreateCloudTableClient();
+
+            CloudTable _table = _table_client.GetTableReference(table_name);
+
+            _table.CreateIfNotExists();
+
+            List<Customer> _customers = new List<Customer>()
+            {
+                new Customer("UserB", "Chicago", "C2"),
+                new Customer("UserC", "Chicago", "C3"),
+                new Customer("UserD", "Chicago", "C4")
+            };
+
+            TableBatchOperation _operation = new TableBatchOperation();
+            foreach(Customer _customer in _customers)
+            {
+                _operation.Insert(_customer);
+            }
+
+            TableBatchResult _result = _table.ExecuteBatch(_operation);
+
+            Console.WriteLine("Entity is added");
+
+            Console.ReadKey();
+        }
+    }
+}
